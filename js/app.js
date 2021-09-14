80b5abe7edc1291682d5ba80c92024d160d04155
Shanto1185 <najmulsketchshanto1185@gmail.com>
2021-09-14 14:29:20 +0600

    fixed bug

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <p> <strong>Ratings: <i class="fas fa-star"></i> ${product.rating.rate} ( ${product.rating.count})</strong></p>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
